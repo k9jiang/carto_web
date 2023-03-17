a342524cc4e24fe35f697b6060c6cbcafe854bb9
k9jiang <kevin.jiang@outlook.Fr>
2023-03-17 16:51:37 +0100

    Update views.sql

--- sql/views.sql
+++ sql/views.sql
@@ -1,4 +1,5 @@
-CREATE view best_discipline_by_country AS
+--Best discipline by country
+CREATE VIEW best_discipline_by_country AS
     WITH tab AS (SELECT country.name, count(medal.medal) AS medalcount, event.discipline
     FROM athlete
         JOIN medal on athlete.id=medal.athlete_id
@@ -7,3 +8,48 @@ CREATE view best_discipline_by_country AS
     GROUP BY country.id, event.discipline),
     tab2 AS (SELECT max(tab.medalcount), name FROM tab GROUP BY name)
 SELECT tab.name, tab.medalcount, tab.discipline FROM tab, tab2 WHERE tab2.max = tab.medalcount AND tab.name = tab2.name;
+
+--Get the 20 best countries in a discipline
+SELECT country.name, count(medal.medal) AS medalcount, event.discipline
+FROM athlete
+JOIN medal ON athlete.id = medal.athlete_id
+JOIN event ON medal.event_id = event.id
+JOIN country ON athlete.country_id = country.id
+WHERE event.discipline = 'Swimming'
+GROUP BY event.discipline, country.name
+ORDER BY medalcount desc limit 20;
+
+--Get the 20 best athletes of a country in a discipline (
+SELECT athlete.name, country.name as nationality, count(medal.medal) AS medalcount, event.discipline
+FROM athlete
+JOIN medal ON athlete.id = medal.athlete_id
+JOIN event ON medal.event_id = event.id
+JOIN country ON athlete.country_id = country.id
+WHERE event.discipline = 'Swimming' AND country.name = 'Australia'
+GROUP BY event.discipline, country.name, athlete.name
+ORDER BY medalcount desc limit 20;
+
+--Get the 20 best athletes of a discipline in the world (change the WHERE clause)
+SELECT athlete.name, country.name as nationality, count(medal.medal) AS medalcount, event.discipline
+FROM athlete
+JOIN medal ON athlete.id = medal.athlete_id
+JOIN event ON medal.event_id = event.id
+JOIN country ON athlete.country_id = country.id
+WHERE event.discipline = 'Swimming'
+GROUP BY event.discipline, country.name, athlete.name
+ORDER BY medalcount desc limit 20;
+
+--Get all the discipline with no duplicate value
+SELECT distinct discipline from event;
+
+SELECT country.name, count(medal.medal) AS medalcount, event.discipline, olympiad.year
+FROM athlete
+JOIN medal ON athlete.id = medal.athlete_id
+JOIN event ON medal.event_id = event.id
+JOIN country ON athlete.country_id = country.id
+JOIN olympiad ON medal.olympiad_id = olympiad.id
+WHERE event.discipline = 'Swimming' AND olympiad.year = '2012'
+GROUP BY event.discipline, country.name, olympiad.year
+ORDER BY medalcount desc;
+
+--SELECT * from event join medal on medal.event_id = event.id where event.name = '4X200M Freestyle';
