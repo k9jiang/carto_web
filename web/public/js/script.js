47cf968e9f5e435ec0be4e5c48a74291271b6b2c
Quentintch <qtchanwou@gmail.com>
2023-03-21 14:22:28 +0100

    Envoi des paramètres côté client

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -7,7 +7,11 @@ const firstYear = document.querySelector("option[value='2014']");
 let reqDiscipline = "disciplines";
 let reqYear = "allYears";
 
+let group;
+
 let filter;
+let map = L.map('map-view').setView([0, 0], 3);
+
 
 //Fonctions
 function updateTitle(){
@@ -22,45 +26,62 @@ function updateTitle(){
     }else{
         spanTitleMap.textContent += ` en ${reqDiscipline}`
     }
-    
 }
 
-function updateGeom(){
+function updateGeom(replace = false){
     if(reqYear == 'allYears'){
         filter = "";
     }else{
-        filter = `&CQL_FILTER=first_participation<=${reqYear}%20AND%20last_participation>=${reqYear}`
+        filter = `&CQL_FILTER=first_participation<=${reqYear}%20AND%20last_participation>=${reqYear}`;
     }
-
+    const url = "http://localhost:8080/geoserver/Carthageo/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Carthageo%3Acountry&outputFormat=application%2Fjson"
+              + filter;
     //Affichage des pays
-    fetch(`http://localhost:8080/geoserver/Carthageo/ows?service=WFS&version=1.0.0&request=GetFeature
-    &typeName=Carthageo%3Acountry
-    &outputFormat=application%2Fjson
-    ${filter}`)
+    fetch(url)
         .then(result => result.json())
         .then(result => {
-            L.geoJSON(result).addTo(map);
+            if(replace){
+                group.clearLayers();
+            }
+            group = L.geoJSON(result).addTo(map);
         })
         .catch(function(error) {
-            console.error(error.message);
+            console.error(error);
         });
+}
+
+function updateData(){
+    //Cercles proportionnelles
+    fetch("http://localhost:3000/data",{
+        method : "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"discipline": reqDiscipline, "year": reqYear})
+    })
+        .then(result => result.json())
+        .then(result => {
+            console.log(result);
+        })
 }
 
 
 //Valeurs sélectionnées de départ
-
 firstDiscipline.setAttribute("checked", true)
 firstYear.setAttribute("selected", true)
 
 //Mise à jour de la carte
 updateTitle();
 updateGeom();
+updateData();
 
 //Interaction avec les disciplines
 $("#chooseADiscipline").change(function(){
     reqDiscipline = this.discipline.value;
 
     updateTitle();
+    updateData();
 })
 
 //Interaction avec les années
@@ -71,24 +92,22 @@ $("#chooseAYear").change(function(){
     if(this.year.value == 'year'){
         selectYear.removeAttribute("disabled");
         reqYear = this.listOfYear.value;
-
     }else{
         reqYear = this.year.value;
     }
 
     updateTitle();
-    updateGeom();
+    updateGeom(true);
+    updateData();
 })
 
 //Affichage du fond de carte carte
-var map = L.map('map-view').setView([48.85, 2.09], 3);
 L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.{ext}', {
     subdomains: 'abcd',
-    minZoom: 0,
+    minZoom: 3,
     maxZoom: 20,
     ext: 'png',
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
 
 
