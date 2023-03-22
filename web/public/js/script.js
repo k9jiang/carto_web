705626ec636290624f72993af3320b4e7a1d4897
k9jiang <kevin.jiang@outlook.Fr>
2023-03-22 10:25:46 +0100

    lil modif

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -62,7 +62,7 @@ function updateMedals(json_query){
         }
         console.log(circles_group);
         circles_group.addTo(map); //displaying features group in the map
-    })
+    })}
 
 function updateCountry(countryName){
     reqCountry = countryName;
@@ -193,5 +193,4 @@ L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-  //Intéraction avec la carte
-
+//Intéraction avec la carte
