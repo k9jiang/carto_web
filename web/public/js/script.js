2e0e680ce3bfde1aa4ced6bff9b0b90151be7ab3
Quentintch <qtchanwou@gmail.com>
2023-03-24 16:17:41 +0100

    Pas grand chose pour chartjs

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -136,6 +136,7 @@ function updateGraph(result, graphic){
         data: {
             labels: name,
             datasets: [{
+                name: name,
                 label: 'Nombre de médaille remporté',
                 data: medalcount,
                 borderWidth: 1
@@ -146,16 +147,15 @@ function updateGraph(result, graphic){
                 y: {
                     beginAtZero: true
                 }
-            },
-            onClick: (e) => {
-                //console.log(myChart.getElementsAtEventForMode(e, 'point', graphCountries.options));
-                //console.log(this)
-                //updateCountry(this); //insertion du nom du pays
             }
+            /*onClick: (e) => {
+                //console.log(myChart.getElementsAtEventForMode(e, 'point', graphCountries.options));
+                console.log(this);
+                //updateCountry(this); //insertion du nom du pays
+            }*/
         }
     });
 }
-
 
 function updateAthletesData(country, discipline, year){
     fetch(`http://localhost:3000/athletes/?country=${country}&discipline=${discipline}&year=${year}`)
@@ -179,7 +179,7 @@ function updateAthletesData(country, discipline, year){
                     spanTitleGraph2[0].textContent = "20 premiers";
                 }
                 noDataAthlete.style.display = "none";
-                athletesChart = updateGraph(res, graphAthlete)
+                athletesChart = updateGraph(res, graphAthlete);
             }
             //updateGraph(res, graphAthlete)
         })
